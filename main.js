5707374371b5b692e609b7ff56fb8330ae115c65
luizhenriqueoliveira085 <luizhenriqueoliveira085@gmail.com>
2025-03-28 20:41:47 +0000

    Finalmente é possível calcular tudo que o projeto se propõe

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 let bill = 0
 let tipPercentage = 0
 let numberOfPeople = 0
+let buttonSelected = null
 
 //receber o valor da conta
-let billInput = document.querySelector('#bill')
-billInput.addEventListener('input', receiveBillValue)
+let billInput = document.querySelector("#bill")
+billInput.addEventListener("input", receiveBillValue)
 
 function receiveBillValue() {
     bill = billInput.valueAsNumber
@@ -12,8 +13,8 @@ function receiveBillValue() {
 }
 
 //receber o número de pessoas 
-let numberOfPeopleInput = document.querySelector('#people')
-numberOfPeopleInput.addEventListener('input', receiveNumberOfPeople)
+let numberOfPeopleInput = document.querySelector("#people")
+numberOfPeopleInput.addEventListener("input", receiveNumberOfPeople)
 
 function receiveNumberOfPeople() {
     numberOfPeople = numberOfPeopleInput.valueAsNumber
@@ -21,13 +22,42 @@ function receiveNumberOfPeople() {
 }
 
 function receiveTipPercentage(value) {
-    let buttonSelected = document.querySelector("button")
-    
+    if (buttonSelected !== null) {
+        buttonSelected.classList.remove("button-selected")
+    }
+
+    buttonSelected = document.querySelector(`#button-${value}`)
+    buttonSelected.classList.add("button-selected")
+    tipPercentage = value / 100
+
+    let customTipInput = document.querySelector ("#custom-tip")
+    customTipInput.value = ""
+
+    calculate()
+}
+
+function receiveCustomTipPercentage() {
+    let customTipInput = document.querySelector("#custom-tip")
+    tipPercentage = customTipInput.valueAsNumber / 100 
+
+    if (buttonSelected !== null) {
+        buttonSelected.classList.remove("button-selected")
+        buttonSelected = null
+    }
+
+
+    calculate()
 }
 
 function calculate() {
     if (bill !== 0 && tipPercentage !== 0 && numberOfPeople !== 0) {
-        console.log("calcular")
+        let tipAmountStrong = document.querySelector(".amount strong")
+        let tipAmountPerson = (bill * tipPercentage) / numberOfPeople
+        tipAmountStrong.innerText = `$ ${tipAmountPerson.toFixed(2)}`
+
+        let totalStrong = document.querySelector(".total strong")
+        let total = (bill / numberOfPeople) + tipAmountPerson
+        totalStrong.innerText = `$ ${total.toFixed(2)}`
     } else {
         console.log("ainda não é possível calcular")
     }
